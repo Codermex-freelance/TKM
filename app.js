98f8b727492f8170044e069955574f5c4b58941c
MLopez <codermex.web@gmail.com>
2024-03-20 09:13:49 -0300

    agregamos funcionalidad al seleccionar un teclado y lo guardamos en el localStorage

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-function cambiarURL() {
-  // Obtiene la URL actual
-    const urlActual = window.location.href;
-    console.log(urlActual);
-    // Reemplaza la parte de la URL que deseas cambiar
-    const nuevaURL = urlActual.replace("ejemplo.com", "nuevodominio.com");
-    console.log(nuevaURL);
-    // Redirige a la nueva URL
-    window.location.href = nuevaURL;
+const teclado = document.querySelector('.articulo__contenido');
+let tecladoMostrar = [];
+
+cargarEventListeners();
+function cargarEventListeners(){
+  //teclado.addEventListener('click', agregarTeclado);
+  
+  const articulos = document.querySelectorAll('.articulo__contenido');
+
+  articulos.forEach(articulo => {
+    articulo.addEventListener('click', agregarTeclado);
+  });
+
+  //Muestra los teclados de Local Storage
+  document.addEventListener('DOMContentLoaded', () => {
+    tecladoMostrar = JSON.parse(localStorage.getItem('teclado')) || [];
+  });
+    
+}
+
+//FUNCIONES
+function agregarTeclado(e){
+  //e.preventDefault();
+  if(e.target.classList.contains('articulo__imagen')){//Valida que solo se ejecute cuando toque esa clase
+      const tecladoSeleecionado = e.target.parentElement.parentElement;//accedemos a todo el div que tiene el contenido del curso
+      leerDatosTeclados(tecladoSeleecionado);
+  }   
+}
+
+function leerDatosTeclados(teclado){
+  const infoTeclado = {
+    nombre: teclado.querySelector('h2').textContent,
+    precio: teclado.querySelector('h3').textContent,
+    id: teclado.querySelector('a').getAttribute('data-id')
+  }
+  //some = permite iterarsobre un arreglo de objetos y verificar si un elemento existe en el
+  const existe = tecladoMostrar.some( teclado => teclado.id === infoTeclado.id);
+  if( existe ){
+    tecladoMostrar = [...teclado];
+  }else{
+    tecladoMostrar = [...tecladoMostrar, infoTeclado];
+  }
+  sincronizarStorage();
+}
+
+function sincronizarStorage(){
+    localStorage.setItem('teclado', JSON.stringify(tecladoMostrar));
 }
